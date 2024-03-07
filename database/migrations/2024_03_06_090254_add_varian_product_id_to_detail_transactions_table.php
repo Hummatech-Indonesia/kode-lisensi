1c043738d457087d5973f480d509212306077360
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-03-07 11:54:39 +0700

    Update 2024_03_06_090254_add_varian_product_id_to_detail_transactions_table.php

--- database/migrations/2024_03_06_090254_add_varian_product_id_to_detail_transactions_table.php
+++ database/migrations/2024_03_06_090254_add_varian_product_id_to_detail_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('detail_transactions', function (Blueprint $table) {
-            $table->foreignUuid('varian_product_id')->constrained()->restrictOnDelete()->nullable()->after('product_id');
+            $table->foreignUuid('varian_product_id')->nullable()->constrained();
         });
     }
 
