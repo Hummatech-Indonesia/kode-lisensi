a3e55f566182560f7f117132a008a0f28189af2a
frendikawa <frsnaifla@gmail.com>
2024-03-20 11:19:37 +0700

    dashboard reseller,notifikasi

--- resources/views/dashboard/pages/reseller-dashboard/notification.blade.php
+++ resources/views/dashboard/pages/reseller-dashboard/notification.blade.php
@@ -16,47 +16,44 @@
         $notifications = NotificationHelper::take($take);
         $totalNotifications = NotificationHelper::count();
     @endphp
-    <div class="" aria-labelledby="pills-wishlist-tab">
+    <div class="container-fluid" aria-labelledby="pills-wishlist-tab">
         <div class="dashboard-order">
             <div class="title">
                 <h2>Notifikasi Saya</h2>
-                
             </div>
             @if ($totalNotifications > 0)
-                <div class="col-xxl-12 mb-4">
-                    <div class="col-xxl-4">
+                <div class="row mb-4">
+                    <div class="col-12">
                         <form method="POST" action="{{ route('notification.markAsRead', $take) }}">
                             @csrf
-                            <button class="btn theme-bg-color btn-md fw-bold mt-4 text-white">
-                                Tandai
-                                Semua Telah Dibaca
+                            <button class="btn btn-primary btn-md fw-bold mt-4">
+                                Tandai Semua Telah Dibaca
                             </button>
                         </form>
-
                     </div>
                 </div>
             @endif
 
             <div class="order-contain">
                 @forelse($notifications as $notify)
-                    <div class="order-box dashboard-bg-box">
-                        <div class="order-container">
-                            <div class="order-icon">
-                                <i data-feather="help-circle"></i>
-                            </div>
-
-                            <div class="order-detail">
-                                <h4>{{ $notify->data['name'] }} </h4>
-                                <h6 class="text-content">
-                                    {{ Carbon::parse($notify->created_at)->translatedFormat('d F Y h:i') }}
-                                </h6>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-auto">
+                                    <i data-feather="help-circle"></i>
+                                </div>
+                                <div class="col">
+                                    <h4>{{ $notify->data['name'] }} </h4>
+                                    <h6 class="text-muted">
+                                        {{ Carbon::parse($notify->created_at)->translatedFormat('d F Y h:i') }}
+                                    </h6>
+                                </div>
                             </div>
                         </div>
                     </div>
                 @empty
                     <p>Belum ada notifikasi terbaru.</p>
                 @endforelse
-
             </div>
         </div>
     </div>
