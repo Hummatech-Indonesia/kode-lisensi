b9df4679c5ffac8b227421ddd5da6e657963777a
frendikawa <frsnaifla@gmail.com>
2024-03-21 11:23:14 +0700

    teks delete rekomendasi produk

--- resources/views/components/delete-product-recommendation.blade.php
+++ resources/views/components/delete-product-recommendation.blade.php
@@ -2,14 +2,14 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Konfirmasi Penghapusan Produk rekomendasi</h5>
+                <h5 class="modal-title">Konfirmasi hapus rekomendasi produk</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="" id="deleteProductRecommendation" method="POST">
                 @csrf
                 @method('DELETE')
                 <div class="modal-body m-3">
-                    <p class="mb-0">Data yang dihapus tidak dapat dikembalikan</p>
+                    <p class="mb-0">Apakah anda yakin ingin menghapus produk ini dari rekomendasi produk</p>
                 </div>
                 <div class="modal-footer">
                     <button type="submit" class="btn btn-secondary">Hapus</button>
