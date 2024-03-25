24ca8a184000ec58887f2ae2ddb048240f31e28d
frendikawa <frsnaifla@gmail.com>
2024-03-25 09:24:07 +0700

    fix

--- app/Http/Controllers/AdminWithdrawalController.php
+++ app/Http/Controllers/AdminWithdrawalController.php
@@ -8,12 +8,6 @@ use Illuminate\Http\Request;
 class AdminWithdrawalController extends Controller
 {
 
-    private static BalanceWithdrawalInterface $withdrawal;
-
-    public function __construct(BalanceWithdrawalInterface $withdrawal){
-        $this->withdrawal=$withdrawal;
-    }
-
     public function index(Request $request)
     {
         return view('dashboard.pages.admin-withdrawal.index');
