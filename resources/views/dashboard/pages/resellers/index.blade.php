e0c92a03573d006359ad6cd455ba56b92b8c6939
mohamadarif03 <pecintarealmadrid2@gmail.com>
2024-03-28 10:24:56 +0700

    Update index.blade.php

--- resources/views/dashboard/pages/resellers/index.blade.php
+++ resources/views/dashboard/pages/resellers/index.blade.php
@@ -1,6 +1,6 @@
 @extends('dashboard.layouts.app')
 @section('css')
-    <link href="{{ asset('dashboard_assets/css/datatables.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('dashboard_assets/css/datatables.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
     <div class="card card-table">
@@ -12,14 +12,14 @@
             <div class="table-responsive table-product">
                 <table class="table theme-table" id="table_id">
                     <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>User</th>
-                        <th>Telepon</th>
-                        <th>Jumlah</th>
-                        <th>Total</th>
-                        <th>Tanggal Registrasi</th>
-                    </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>User</th>
+                            <th>Telepon</th>
+                            <th>Jumlah</th>
+                            <th>Total</th>
+                            <th>Tanggal Registrasi</th>
+                        </tr>
                     </thead>
                     <tbody>
                     </tbody>
@@ -27,12 +27,11 @@
             </div>
         </div>
     </div>
-
 @endsection
 @section('script')
     <script src="{{ asset('dashboard_assets/js/jquery.dataTables.js') }}"></script>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
 
 
             // Datatables Responsive
@@ -46,12 +45,12 @@
                 processing: true,
                 serverSide: true,
                 searching: true,
-                ajax: "{{ route('users.reseller.index') }}",
+                ajax: "/dashboard/users/customer",
                 columns: [{
-                    data: 'DT_RowIndex',
-                    orderable: false,
-                    searchable: false
-                },
+                        data: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false
+                    },
                     {
                         data: 'name',
                         name: 'name'
