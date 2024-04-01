a2636873f83ea081c3eb651884ca5c33eee2f522
frendikawa <frsnaifla@gmail.com>
2024-04-01 14:52:55 +0700

    bug fix

--- resources/views/dashboard/pages/products/index.blade.php
+++ resources/views/dashboard/pages/products/index.blade.php
@@ -77,7 +77,7 @@
         $(document).ready(function() {
             var table = $("#table_id").DataTable({
                 scrollX: false,
-                scrollY: '500px',z
+                scrollY: '500px',
                 paging: true,
                 ordering: true,
                 responsive: true,
