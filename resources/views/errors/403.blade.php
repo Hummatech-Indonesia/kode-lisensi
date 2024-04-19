cc7a8723f6ed4d60cd78e8bf48f9a7fd2a51edb8
frendikawa <frsnaifla@gmail.com>
2024-04-19 20:45:02 +0700

    halaman 403

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,1 +1,27 @@
-Forbidden Access
+@extends('layouts.main')
+@section('content')
+    <section class="section-404 section-lg-space">
+        <div class="container-fluid-lg">
+            <div class="row">
+                <div class="col-12">
+                    <div class="image-404">
+                        <img src="{{ asset('assets/images/inner-page/403.png') }}" class="img-fluid blur-up lazyloaded"
+                             alt="">
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="contain-404">
+                        <h3 class="text-content">Anda tidak memiliki izin untuk mengakses halaman ini.</h3>
+                        <div class="col-md-12">
+                            <a href="{{ route('home.index') }}"
+                               class="btn btn-md text-white theme-bg-color mt-4 mx-auto">Kembali ke Beranda
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
