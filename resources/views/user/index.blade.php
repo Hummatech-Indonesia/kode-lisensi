746c94993f928348e4a97a93c6a97e2184717494
frendikawa <frsnaifla@gmail.com>
2024-05-03 10:00:06 +0700

    riwayat pengajuan

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,6 +2,7 @@
     use App\Enums\InvoiceStatusEnum;
     use App\Enums\ProductStatusEnum;
     use App\Enums\UserRoleEnum;
+    use App\Enums\StatusRefundEnum;
     use App\Helpers\CurrencyHelper;
     use App\Helpers\RatingHelper;
     use App\Helpers\UserHelper;
@@ -81,6 +82,14 @@
                                     aria-selected="false">
                                     <i data-feather="shopping-bag"></i>
                                     Riwayat Transaksi
+                                </button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="pills-refund-tab" data-bs-toggle="pill"
+                                    data-bs-target="#pills-refund" type="button" role="tab"
+                                    aria-controls="pills-refund" aria-selected="false">
+                                    <i data-feather="shopping-bag"></i>
+                                    Riwayat Pengajuan
                                 </button>
                             </li>
 
@@ -344,7 +353,8 @@
                                                             @if (
                                                                 $trans->invoice_status == InvoiceStatusEnum::PAID->value ||
                                                                     $trans->invoice_status == InvoiceStatusEnum::SETTLED->value)
-                                                                <a title="Ajukan Pengembalian Dana" id="return" data-id="{{$trans->id}}"
+                                                                <a title="Ajukan Pengembalian Dana" id="return"
+                                                                    data-id="{{ $trans->id }}"
                                                                     class="btn btn-warning btn-sm text-white">
                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16"
                                                                         height="16" fill="currentColor"
@@ -451,6 +461,58 @@
                                     </div>
                                 </div>
                             </div>
+
+                            {{-- profil saya --}}
+                            <div class="tab-pane fade" id="pills-refund" role="tabpanel"
+                                aria-labelledby="pills-refund-tab">
+                                <div class="dashboard-profile">
+                                    <div class="title">
+                                        <h2>Refund</h2>
+                                        <span class="title-leaf">
+                                            <svg class="icon-width bg-gray">
+                                                <use xlink:href="../assets/svg/leaf.svg#leaf"></use>
+                                            </svg>
+                                        </span>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <div class="title-header option-title">
+                                            <h5>Halaman Permintaan Pengajuan Dana Kembali</h5>
+                                        </div>
+                                        <div class="d-flex mb-3 justify-content-between">
+
+                                            <div style="width: 200px">
+                                                <select name="" class="form-select" id="status">
+                                                    <option value="">Tampilkan Semua</option>
+                                                    <option value="{{ StatusRefundEnum::ACCEPTED }}">Diterima</option>
+                                                    <option value="{{ StatusRefundEnum::REJECT }}">Ditolak</option>
+                                                    <option value="{{ StatusRefundEnum::PENDING }}">Diproses</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="table-responsive table-product">
+                                            <table class="table theme-table" id="table_id">
+                                                <thead>
+                                                    <tr>
+                                                        {{-- <th>Penggunaan</th>
+                                                        <th>Penarikan Melalui</th> --}}
+                                                        <th>Bukti</th>
+                                                        <th>Product Dibeli</th>
+                                                        <th>Dana Diajukan</th>
+                                                        <th>Deskripsi</th>
+                                                        <th>Status</th>
+                                                        <th>Tanggal Penarikan</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
                             {{-- profil saya --}}
                             <div class="tab-pane fade" id="pills-profile" role="tabpanel"
                                 aria-labelledby="pills-profile-tab">
@@ -596,6 +658,8 @@
 @endsection
 @section('script')
     <x-add-refund-modal></x-add-refund-modal>
+    <x-reject-refund-modal></x-reject-refund-modal>
+    <x-approve-refund-modal></x-approve-refund-modal>
 
     <script>
         $(document).on('click', '#return', function() {
@@ -605,5 +669,77 @@
             let url = `{{ route('dashboard.refund.store', ':id') }}`.replace(':id', id);
             $('#addRefund').attr('action', url);
         });
+    </script>
+
+
+
+    <script src="{{ asset('dashboard_assets/js/jquery.dataTables.js') }}"></script>
+    <script>
+        $(document).on('click', '#approveRefund', function() {
+            $('#approveRefundModal').modal('show')
+            const id = $(this).attr('data-id');
+            console.log(id);
+            console.log('test');
+            let url = `{{ route('dashboard.refund.approve', ':id') }}`.replace(':id', id);
+            $('#formApprove').attr('action', url);
+        });
+
+        $.ajax({
+            type: "method",
+            url: "url",
+            data: "data",
+            dataType: "dataType",
+            success: function(response) {}
+        });
+
+        var table = $("#table_id").DataTable({
+            scrollX: false,
+            scrollY: '500px',
+            paging: true,
+            ordering: true,
+            responsive: true,
+            pageLength: 50,
+            processing: true,
+            serverSide: false,
+            searching: true,
+            ajax: {
+                url: "{{ route('dashboard.refund.my.refund') }}",
+                data: function(d) {
+                    d.status = $('#status').val();
+                }
+            },
+            columns: [{
+                    data: 'proof',
+                    name: 'proof'
+                },
+                {
+                    data: 'product',
+                    name: 'product'
+                },
+                {
+                    data: 'balance',
+                    name: 'balance'
+                },
+                {
+                    data: 'description',
+                    name: 'description'
+                },
+                {
+                    data: 'status',
+                    name: 'status'
+                },
+                {
+                    data: 'created_at',
+                    name: 'created_at'
+                },
+            ]
+        });
+        $('#status').on('change', function() {
+            console.log($('#status').val());
+            table.ajax.reload();
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 @endsection
