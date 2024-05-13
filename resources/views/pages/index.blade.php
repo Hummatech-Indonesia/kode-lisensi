2155547f1a4834a77a6053294c795999b9d4efb4
frendikawa <frsnaifla@gmail.com>
2024-05-13 08:51:30 +0700

    foto rekomendasi produk

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -88,10 +88,10 @@
                                     style="visibility: visible; animation-name: fadeInUp;">
                                     <div class="product-header">
                                         <div class="product-image">
-                                            <h4><a class="badge bg-success p-1 text-white"
-                                                href="{{ route('home.category', $product->category->slug) }}">{{ $product->category->name }}</a>
-                                        </h4>
-
+                                            <a href="{{ route('home.products.show', $product->slug) }}">
+                                                <img src="{{ asset('storage/' . $product->photo) }}"
+                                                    class="img-fluid blur-up lazyloaded" alt="">
+                                            </a>
                                             <ul class="product-option">
                                                 <li data-bs-toggle="tooltip" data-bs-placement="top" title=""
                                                     data-bs-original-title="Lihat Produk">
@@ -102,14 +102,14 @@
 
                                                 <li data-bs-toggle="tooltip" data-bs-original-title="Bagikan Produk">
                                                     @if (UserHelper::getUserRole() == UserRoleEnum::RESELLER->value)
-                                                        <a href="#">
+                                                        <a href="#" onclick="return false;">
                                                             <i data-feather="share-2" data-bs-toggle="modal"
                                                                 data-bs-target="#shareProductModal"
                                                                 data-slug="{{ $product->slug }}" id="shareButtonsTrigger"
                                                                 data-code="{{ auth()->user()->code_affiliate }}"></i>
                                                         </a>
                                                     @else
-                                                        <a href="#">
+                                                        <a href="#" onclick="return false;">
                                                             <i data-feather="share-2" data-bs-toggle="modal"
                                                                 data-bs-target="#shareProductModal"
                                                                 data-slug="{{ $product->slug }}"
@@ -117,7 +117,6 @@
                                                         </a>
                                                     @endif
                                                 </li>
-
                                                 @auth
                                                     @if ($product->product_favorites->where('user_id', auth()->user()->id)->first())
                                                         <li data-bs-toggle="tooltip" class="favorite" data-bs-placement="top"
@@ -152,13 +151,11 @@
                                                     <li data-bs-toggle="tooltip" data-bs-placement="top" title=""
                                                         data-bs-original-title="Favorit">
                                                         <a href="/login" class="add-favorite" data-id="{{ $product->id }}">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                                height="16" fill="currentColor" class="bi bi-suit-heart"
-                                                                viewBox="0 0 16 16">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                                fill="currentColor" class="bi bi-suit-heart" viewBox="0 0 16 16">
                                                                 <path
                                                                     d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.6 7.6 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
                                                             </svg>
-
                                                         </a>
                                                     </li>
                                                 @endguest
@@ -167,7 +164,7 @@
                                     </div>
                                     <div class="product-footer">
                                         <div class="product-detail">
-                                            <h4><a
+                                            <h4><a class="badge bg-success p-1 text-white"
                                                     href="{{ route('home.category', $product->category->slug) }}">{{ $product->category->name }}</a>
                                             </h4>
                                             <a href="{{ route('home.products.show', $product->slug) }}">
