188fbbab7d37fe9cb6dc507d17d6efb3e2a455c5
frendikawa <frsnaifla@gmail.com>
2024-05-22 10:57:16 +0700

    push
    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -145,114 +145,119 @@
         </div>
     </div>
 </div>
-<div class="container-fluid-lg">
-    <div class="row">
-        <div class="col-12">
-            <div class="main-nav">
-                <div class="header-nav-left">
-                    <button class="dropdown-category dropdown-category-2">
-                        <i class="iconly-Category icli"></i>
-                        <span>Semua Kategori</span>
-                    </button>
-                    <div class="category-dropdown">
-                        <div class="category-title">
-                            <h5>Categories</h5>
-                            <button type="button" class="btn p-0 close-button text-content">
-                                <i class="fa-solid fa-xmark"></i>
-                            </button>
-                        </div>
+<div class="top-header top-nav sticky-header sticky-header-3">
 
-                        <ul class="category-list">
-                            @foreach ($nav_categories as $category)
-                                <li class="onhover-category-list">
-                                    <a href="{{ route('home.category', $category->slug) }}" class="category-name">
-                                        <h6>{{ $category->name }}</h6>
-                                        <i class="fa-solid fa-angle-right"></i>
-                                    </a>
-                                    <div class="onhover-category-box">
-                                        <div class="list-1">
-                                            <div class="category-title-box">
-                                                <h5>{{ $category->name }}</h5>
-                                            </div>
-                                            <ul>
-                                                @forelse($category->products as $product)
-                                                    <li>
-                                                        <a
-                                                            href="{{ route('home.products.show', $product->slug) }}">{{ $product->name }}</a>
-                                                    </li>
-                                                @empty
-                                                    Belum ada Produk
-                                                @endforelse
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
+    <div class="container-fluid-lg">
+        <div class="row">
+            <div class="col-12">
+                <div class="main-nav">
+                    <div class="header-nav-left">
+                        <button class="dropdown-category dropdown-category-2">
+                            <i class="iconly-Category icli"></i>
+                            <span>Semua Kategori</span>
+                        </button>
+                        <div class="category-dropdown">
+                            <div class="category-title">
+                                <h5>Categories</h5>
+                                <button type="button" class="btn p-0 close-button text-content">
+                                    <i class="fa-solid fa-xmark"></i>
+                                </button>
+                            </div>
 
-                <div class="main-nav navbar navbar-expand-xl navbar-light navbar-sticky">
-                    <div class="offcanvas offcanvas-collapse order-xl-2" id="primaryMenu">
-                        <div class="offcanvas-header navbar-shadow">
-                            <h5>Menu</h5>
-                            <button class="btn-close lead" type="button" data-bs-dismiss="offcanvas"
-                                aria-label="Close"></button>
-                        </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link {{ request()->routeIs('home.index') ? 'active' : '' }}"
-                                        href="{{ route('home.index') }}">Beranda</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link {{ request()->routeIs('home.products.*') ? 'active' : '' }}"
-                                        href="{{ route('home.products.index') }}">Produk</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link {{ request()->routeIs('home.about') ? 'active' : '' }}"
-                                        href="{{ route('home.about') }}">Tentang Kami</a>
-                                </li>
-                                <li class="nav-item dropdown new-nav-item">
-                                    {{-- <a class="nav-link dropdown-toggle" href="javascript:void(0)"
-                                        data-bs-toggle="dropdown">Blog</a> --}}
-
-                                    <a class="nav-link dropdown-toggle"
-                                        href="{{ route('home.articles.index') }}">Blog</a>
-                                    <ul class="dropdown-menu">
-                                        @foreach (CategoryHelper::articleCategory() as $articleCategory)
-                                            <li class="sub-dropdown-hover">
-                                                <a class="dropdown-item"
-                                                    href="javascript:void(0)">{{ $articleCategory->name }}</a>
-                                                <ul class="sub-menu">
-                                                    @forelse ($articleCategory->sub_article_categories as $sub_article_category)
+                            <ul class="category-list">
+                                @foreach ($nav_categories as $category)
+                                    <li class="onhover-category-list">
+                                        <a href="{{ route('home.category', $category->slug) }}"
+                                            class="category-name">
+                                            <h6>{{ $category->name }}</h6>
+                                            <i class="fa-solid fa-angle-right"></i>
+                                        </a>
+                                        <div class="onhover-category-box">
+                                            <div class="list-1">
+                                                <div class="category-title-box">
+                                                    <h5>{{ $category->name }}</h5>
+                                                </div>
+                                                <ul>
+                                                    @forelse($category->products as $product)
                                                         <li>
                                                             <a
-                                                                href="{{ route('home.articles.index') . '?sub_category=' . $sub_article_category->name }}">{{ $sub_article_category->name }}</a>
+                                                                href="{{ route('home.products.show', $product->slug) }}">{{ $product->name }}</a>
                                                         </li>
                                                     @empty
-                                                        <p style="font-size: 13px">Belum mempunyai Sub-Kategori</p>
+                                                        Belum ada Produk
                                                     @endforelse
                                                 </ul>
-                                            </li>
-                                        @endforeach
-
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link {{ request()->routeIs('home.contact.index') ? 'active' : '' }}"
-                                        href="{{ route('home.contact.index') }}">Hubungi Kami</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link {{ request()->routeIs('home.faq') ? 'active' : '' }}"
-                                        href="{{ route('home.faq') }}">Bantuan</a>
-                                </li>
-
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endforeach
                             </ul>
                         </div>
                     </div>
-                </div>
 
+                    <div class="main-nav navbar navbar-expand-xl navbar-light navbar-sticky">
+                        <div class="offcanvas offcanvas-collapse order-xl-2" id="primaryMenu">
+                            <div class="offcanvas-header navbar-shadow">
+                                <h5>Menu</h5>
+                                <button class="btn-close lead" type="button" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ request()->routeIs('home.index') ? 'active' : '' }}"
+                                            href="{{ route('home.index') }}">Beranda</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ request()->routeIs('home.products.*') ? 'active' : '' }}"
+                                            href="{{ route('home.products.index') }}">Produk</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ request()->routeIs('home.about') ? 'active' : '' }}"
+                                            href="{{ route('home.about') }}">Tentang Kami</a>
+                                    </li>
+                                    <li class="nav-item dropdown new-nav-item">
+                                        {{-- <a class="nav-link dropdown-toggle" href="javascript:void(0)"
+                                        data-bs-toggle="dropdown">Blog</a> --}}
+
+                                        <a class="nav-link dropdown-toggle"
+                                            href="{{ route('home.articles.index') }}">Blog</a>
+                                        <ul class="dropdown-menu">
+                                            @foreach (CategoryHelper::articleCategory() as $articleCategory)
+                                                <li class="sub-dropdown-hover">
+                                                    <a class="dropdown-item"
+                                                        href="javascript:void(0)">{{ $articleCategory->name }}</a>
+                                                    <ul class="sub-menu">
+                                                        @forelse ($articleCategory->sub_article_categories as $sub_article_category)
+                                                            <li>
+                                                                <a
+                                                                    href="{{ route('home.articles.index') . '?sub_category=' . $sub_article_category->name }}">{{ $sub_article_category->name }}</a>
+                                                            </li>
+                                                        @empty
+                                                            <p style="font-size: 13px">Belum mempunyai Sub-Kategori</p>
+                                                        @endforelse
+                                                    </ul>
+                                                </li>
+                                            @endforeach
+
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ request()->routeIs('home.contact.index') ? 'active' : '' }}"
+                                            href="{{ route('home.contact.index') }}">Hubungi Kami</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link {{ request()->routeIs('home.faq') ? 'active' : '' }}"
+                                            href="{{ route('home.faq') }}">Bantuan</a>
+                                    </li>
+
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
